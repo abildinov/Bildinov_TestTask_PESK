3b27e39c366f77a90c417c57a7967db1f0fcf808
abildinov <alekseybildinov@yande.ru>
2026-04-14 17:47:35 +0300

    добавил генерацию,проверку jwt

--- app/core/security.py
+++ app/core/security.py
@@ -1,7 +1,69 @@
+from uuid import uuid4
+from datetime import datetime, timedelta, timezone
+from pwdlib import PasswordHash
+import jwt
+
+from app.core.config import get_settings
+
+settings = get_settings()
 
 
-def hash_password(str) -> str:
-    pass
+password_hash = PasswordHash.recommended()
+DUMMY_HASH = password_hash.hash("dummypassword")
 
-def verify_password(plain_password:str, hashed_password: str) -> bool:
-    pass
+
+def hash_password(password) -> str:
+    return password_hash.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return password_hash.verify(plain_password, hashed_password)
+
+
+def create_token(payload: dict, expires_delta: timedelta) -> str:
+    to_encode = payload.copy()
+
+    now: datetime = datetime.now(timezone.utc)
+    expires: datetime = now + expires_delta
+
+    to_encode['iat'] = now # время выпуска
+    to_encode['exp'] = expires # срок жизни
+
+    token: str = jwt.encode(to_encode,
+                       settings.jwt_secret_key.get_secret_value(),
+                       algorithm=settings.jwt_algorithm,
+                       )
+
+    return token
+
+
+def create_access_token(user_id: int, session_id: str, roles: list[str]) -> str:
+    payload = {
+        "sub": str(user_id),
+        "jti": str(uuid4()),
+        "sid": session_id,
+        "type": "access",
+        "roles": roles,
+    }
+    return create_token(payload, timedelta(minutes=settings.access_token_expire_minutes))
+
+
+def create_refresh_token(user_id: int, session_id: str) -> str:
+    payload = {
+        "sub": str(user_id),
+        "jti": str(uuid4()),
+        "sid": session_id,
+        "type": "refresh",
+    }
+    return create_token(payload, timedelta(days=settings.refresh_token_expire_days))
+
+
+def decode_token(token: str) -> dict:
+    return jwt.token(
+        token,
+        settings.jwt_secret_key.get_secret_value(),
+        algorithms=[settings.jwt_algorithm],
+    )
+
+
+
