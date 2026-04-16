26b99d0d079312da005524a0285162fc00ba29e7
abildinov <alekseybildinov@yande.ru>
2026-04-16 15:36:30 +0300

    роуты для авторизации

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -1,3 +1,15 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db.postgres import get_db
+from app.models.user import User
+from app.models.role import Role
+from app.schemas.auth import RegisterRequest
+from app.schemas.user import UserResponse
+from app.core.security import hash_password
+
+
 
 
 
